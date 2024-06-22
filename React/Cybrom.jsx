39d75caa595298572bdfee2ed1484e1e9760c556
Arun silawat <arunsilawat722362@gmail.com>
2024-06-22 13:29:15 +0530

    Cybrom data listing

---
+++ React/Cybrom.jsx
@@ -0,0 +1,41 @@
+
+
+const Cybrom=(props)=>{
+    return(
+        <>
+        <tr>
+            <td>{props.nm}</td>
+            <td>{props.roll}</td>
+            <td>{props.ct}</td>
+            <td>{props.fee}</td>
+        </tr>
+        </>
+    )
+}
+
+export default Cybrom;
+
+//-------------------------------------------------------------------------
+import Stu from "./Stu";
+import Cybrom from "./Cybrom";
+
+
+const ans=Stu.map((key)=><Cybrom nm={key.name} roll={key.rollno} ct={key.city} fee={key.fees}/>)
+
+const App=()=>{
+  return(
+    <>
+    <table bgcolor="yellow" border="1" align="center" width="900" height="200">
+      <tr bgcolor="pink">
+        <th>Name</th>
+        <th>Roll number</th>
+        <th>City</th>
+        <th>Fees</th>
+      </tr>
+      {ans}
+    </table>
+    </>
+  )
+}
+
+export default App;
