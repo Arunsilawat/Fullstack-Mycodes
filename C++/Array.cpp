1c3104db314bcd4f7b1d2368312efda43ab43553
Arun silawat <arunsilawat722362@gmail.com>
2024-06-23 23:50:53 +0530

    Array)

--- C++/Array.cpp
+++ C++/Array.cpp
@@ -127,4 +127,15 @@ int main(){
 
 
 }
- 
+ //------------------------------------------------------------------
+ #include <iostream>
+using namespace std;
+
+int main(){
+	int student[5]{2,3,4,5,6};
+	cout<<student[4];
+	cout<<student[4];
+	cout<<student[4];
+	cout<<student[0];
+
+}
