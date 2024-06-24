00face333c4cdf5c33aedb4c2920eb00445adade
Arun silawat <arunsilawat722362@gmail.com>
2024-06-24 16:47:59 +0530

    react css topic

---
+++ React/Collage copy.jsx
@@ -0,0 +1,42 @@
+
+import Cybrom from "./Cybrom";
+const Collage=()=>{
+    return(
+      <>
+      <h1 className="head1">I am fullstack student</h1>
+      <Cybrom/>
+      </>
+    )
+  
+  }
+  export default Collage;
+
+  //-----------------------------------------------------------------
+  const App=()=>{
+    return(
+      <>
+   <h1>Welcome to Cybrom</h1>
+   <div id="box1">
+    <div id="box2">
+      <div id="box3">
+        <h1>Cybrom</h1>
+      </div>
+    </div>
+   </div>
+    
+     </>
+    )
+  }
+
+  //---------------------------------
+  //  import Collage from "./Collage";
+// const App=()=>{
+//   return(
+//     <>
+//  <h1>Cybrom Student </h1>
+//   <Collage/>
+//    </>
+//   )
+// }
+
+// export default App;
