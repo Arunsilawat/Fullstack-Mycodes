c82056467609936a9ffe3f1dcba58b4a59504926
Arun silawat <arunsilawat722362@gmail.com>
2024-06-27 15:34:09 +0530

    Cast of a pen with discount

--- C++/Oops.cpp
+++ C++/Oops.cpp
@@ -747,3 +747,24 @@ using namespace std;
 //	}
 //	cout<<s;
 //}
+//-------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+int main(){
+	int cost,amount;
+	float discount;
+	cout<<"enter the cost of pen :\n";
+	cin>>cost;
+	amount=cost;
+	if(cost>1000){
+		discount=(20/100.0)*amount;
+		cout<<"the discount is :"<<discount<<" and the amount to be paid is :"<<amount-discount;
+	}
+	else{
+		discount=(10/100.0)*amount;
+		cout<<"the discount is :"<<discount<<" and the amount to be paid is :"<<amount-discount;
+
+	}
+	
+	
+}
