079d56e35858aa7537d13625ad742fb2ed1cc760
Arun silawat <arunsilawat722362@gmail.com>
2024-07-01 14:53:24 +0530

    Route Dom nested

--- React/Route_Bootstrap/App.jsx
+++ React/Route_Bootstrap/App.jsx
@@ -1,24 +1,33 @@
-
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Layout from "./Layout";
-import Home from "./Home";
-import About from "./About";
-import Contant from "./Contant";
-import NoPage from "./NoPage";
+import Home from "./Pages/Home";
+import Service from "./Pages/Service";
+import About from "./Pages/About";
+import NoPage from "./Pages/NoPage";
+import Contact from "./Pages/Contact";
+import CompanyService from "./Pages/CompanyService";
+import ProductService from "./Pages/Productservice";
+import SellsService from "./Pages/SellsService";
+
 const App=()=>{
     return(
         <>
           <BrowserRouter>
-                  <Routes>
-                      <Route path="/" element={<Layout/>}>
-                            <Route index element={<Home/>}></Route>
-                            <Route path="/home" element={<Home/>}/>
-                            <Route path="/about" element={<About/>}/>
-                            <Route path="/contant" element={<Contant/>}/>
-                            <Route path="*" element={<NoPage/>}/>
-                      </Route>
-                  </Routes>
-           </BrowserRouter>       
+             <Routes>
+                  <Route path="/" element={<Layout/>}>
+                       <Route index element={<Home/>}/>
+                       <Route path="/home" element={<Home/>}/>
+                       <Route path="/service" element={<Service/>} >
+                          <Route path="companyservice" element={<CompanyService/>}/>
+                          <Route path="productservice" element={<ProductService/>}/>
+                          <Route path="sellsservice" element={<SellsService/>}/>
+                       </Route>
+                       <Route path="/about" element={<About/>}/>
+                       <Route path="/contact" element={<Contact/>}/>
+                       <Route path="*" element={<NoPage/>}/>
+                  </Route>
+             </Routes>
+          </BrowserRouter>
         </>
     )
 }
