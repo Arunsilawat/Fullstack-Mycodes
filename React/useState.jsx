de6688880dbac9b2e9b0510c86b23272752ce112
Arun silawat <arunsilawat722362@gmail.com>
2024-07-05 16:52:02 +0530

    useState

--- React/useState.jsx
+++ React/useState.jsx
@@ -85,3 +85,34 @@ const myrec=()=>{
   )
 }
 export default App;
+
+//-----------------------------------------------------------------
+import { useState } from "react";
+const App=()=>{
+  const [input, setInput]=useState({});
+  const handleInput=(e)=>{
+       let name=e.target.name;
+       let value=e.target.value;
+       setInput(values=>({...values,[name]:value}));
+       console.log(input);
+  }
+  const handlesubmit=()=>{
+    console.log(input)  
+  }
+//-----------------------------------------------------------------------------
+  return(
+    <>
+    Enter Empno  : <input type="text" name="empno" onChange={handleInput}/>
+    <br />
+    Enter Name   : <input type="text" name="name" onChange={handleInput}/>
+    <br />
+    Enter City   : <input type="text" name="city" onChange={handleInput} />
+    <br />
+    Enter Salary : <input type="text" name="salary" onChange={handleInput}/>
+    <br />
+    <button onClick={handlesubmit}>Save</button>
+    </>
+  )
+}
+// export default App;
+//----------------------------------------------------------------------------------------today
