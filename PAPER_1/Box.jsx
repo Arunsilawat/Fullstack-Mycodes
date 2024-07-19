d3eb8671e84ccf0349e55951e842f84b80de4fe2
Arun silawat <arunsilawat722362@gmail.com>
2024-07-19 15:50:04 +0530

    Box

---
+++ PAPER_1/Box.jsx
@@ -0,0 +1,17 @@
+
+
+const Box=()=>{
+    return(
+        <>
+         
+         <div style={{width:400,height:220,backgroundColor:"yellow",borderRadius:"60%",border:'3px solid black',margin:'auto',marginTop:50}}>
+            <div style={{width:260,height:110,backgroundColor:"blue",border:'3px solid black',margin:'auto',marginTop:50}}>
+                <div style={{width:160,height:80,backgroundColor:"red",borderRadius:"60%",border:'3px solid black',margin:'auto',marginTop:10}}>
+                    <div style={{width:70,margin:'auto',marginTop:20,fontWeight:'bold',color:'white'}}>CYBROM</div>
+                </div>
+            </div>
+         </div>
+        </>
+    )
+}
+export default Box;
