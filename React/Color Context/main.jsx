a42c79104285482ae8b8b83460314c129bca31ba
Arun silawat <arunsilawat722362@gmail.com>
2024-07-23 15:39:19 +0530

    Color Contaxt

---
+++ React/Color Context/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './CSS/style.css'
+import ColorContact from './ColorContaxt.jsx';
+ReactDOM.createRoot(document.getElementById('root')).render(
+<ColorContact>
+<App/>
+</ColorContact>
+)
+
+ 
