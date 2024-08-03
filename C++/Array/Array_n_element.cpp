9ad9cbad4778d77f9c41dbb985a0aa533d3707f8
Arun silawat <arunsilawat722362@gmail.com>
2024-08-03 22:45:58 +0530

    Array with n element

---
+++ C++/Array/Array_n_element.cpp
@@ -0,0 +1,22 @@
+-
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[6]={43,45,66,56,77,65};
+	
+     int i=0;  
+    int num=2;  
+
+     while(i<6){
+        arr[i]=num;
+        num+=2;  
+        i++;
+    }
+     cout<<"Array of even elements: ";
+    i=0;    
+    while(i<6) {
+        cout<<arr[i]<<" ";
+        i++;
+    }
+    cout<<endl;
+}
