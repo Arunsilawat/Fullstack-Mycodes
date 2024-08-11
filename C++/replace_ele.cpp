767f029c23868c4bf7eef6ebfdfa9c20a304eab5
Arun silawat <arunsilawat722362@gmail.com>
2024-08-11 19:39:34 +0530

    Replace element

---
+++ C++/replace_ele.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+	string st="apple is good ";
+	string st1="this is our python class";
+	
+	st.replace(6,2,"are");
+	st1.replace(12,6,"c++");
+	
+	cout<<st1<<endl;
+	cout<<st; 
+
+}
