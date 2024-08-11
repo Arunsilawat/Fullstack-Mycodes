6335fa82537e40bad5c7b4f182428028646aef27
Arun silawat <arunsilawat722362@gmail.com>
2024-08-11 19:38:34 +0530

    Rectangle

---
+++ C++/Rectangle.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+	int l,b;
+	cout<<"Enter length and breadth of a rectangle :";
+	cin>>l>>b;
+	if(l==b)
+	cout<<"The figure is a square ";
+	else
+	cout<<"The figure is not a square ";
+	
+}
