2602a9c6de1dd360d590817ef8052ff2878fde78
Arun silawat <arunsilawat722362@gmail.com>
2024-08-12 15:59:25 +0530

    erase

---
+++ C++/erase.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string st="apple is good ";
+	string st1="this is our python class";
+	
+	st.erase(5,3);  //erase (starting posi,->limit )
+	cout<<st; 
+
+}
