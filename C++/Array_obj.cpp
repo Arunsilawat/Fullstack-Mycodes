6c8032a9ecc354d028a989f326b086ecd1cf6299
Arun silawat <arunsilawat722362@gmail.com>
2024-09-09 22:21:44 +0530

    u

--- C++/Array_obj.cpp
+++ C++/Array_obj.cpp
@@ -3,36 +3,36 @@
 #include <iostream>
 using namespace std;
 //
-//class Student {
-//	public: 
-//	int roll;
-//	string name;
-//	double marks;
-//	
-//	
-//		Student(){
-//			cout<<"Enter Roll no, Name, Marks ";
-//			cin>>roll>>name>>marks;
-//		}
-//};
-//
-//int main(){
-//	int n;
-//	cout<<"How many record do u want to insert : ";
-//	cin>>n;
-//	Student s[n];
-//    
-//	for(int i=0;i<n;i++){
-//		cout<<"name: "<<s[i].name<<endl<<"roll: "<<s[i].roll<<endl<<"marks: "<<s[i].marks<<endl;
-//	}
-//	
-//	int high =s[0].marks;
-//	for(int i=1;i<n;i++){
-//		if(s[i].marks>high)
-//		   high=s[i].marks;
-//	}	
-//	cout<<"highest marks :"<<high;
-//}
+class Student {
+	public: 
+	int roll;
+	string name;
+	double marks;
+	
+	
+		Student(){
+			cout<<"Enter Roll no, Name, Marks ";
+			cin>>roll>>name>>marks;
+		}
+};
+
+int main(){
+	int n;
+	cout<<"How many record do u want to insert : ";
+	cin>>n;
+	Student s[n];
+   
+	for(int i=0;i<n;i++){
+		cout<<"name: "<<s[i].name<<endl<<"roll: "<<s[i].roll<<endl<<"marks: "<<s[i].marks<<endl;
+	}
+	
+	int high =s[0].marks;
+	for(int i=1;i<n;i++){
+		if(s[i].marks>high)
+		   high=s[i].marks;
+	}	
+	cout<<"highest marks :"<<high;
+}
 //-------------
 class Student{
 	public:
