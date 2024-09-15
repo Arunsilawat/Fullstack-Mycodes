b546d7a1248cd334dd39d7bbf8500880f50070e8
Arun silawat <arunsilawat722362@gmail.com>
2024-09-15 23:07:59 +0530

    Map

---
+++ Python/Map.py
@@ -0,0 +1,26 @@
+#map() -->number or input == no of output-->colection return aayega
+
+# my_list=[1,2,3,4,5]
+# def squer(n):
+#     return n**2
+# new_list=list(map(squer,my_list))
+# print(new_list)
+
+
+my_list1=[1,2,3,4,5]
+my_list2=[5,4,3,2,1]
+def add(x,y):
+    return x+y
+new_list=list(map(add,my_list1,my_list2))
+print(new_list)
+#----------------------------------------------------------------
+
+def add(x,y):
+    return x+y
+new_list=list(map(add,[5,4,3,2,1],[1,2,3,4,5]))
+print(new_list)
+
+#------------------------------------------------
+#lambda 
+x=lambda a,b:a+b
+print((list(map(x,[1,2,3,4,5],[5,4,3,2,1]))))
