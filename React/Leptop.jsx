4fb8376468d78a5b20742c3c95fcfbbcbf28b960
Arun silawat <arunsilawat722362@gmail.com>
2024-09-22 23:15:59 +0530

    react

--- React/Leptop.jsx
+++ React/Leptop.jsx
@@ -23,141 +23,141 @@
 // const App=()=>{
 //   return(
 //     <>
-//           <Navbar expand="lg" className="bg-body-tertiary">
-//       <Container fluid>
-//         <Navbar.Brand href="#">Arun Silawat</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="navbarScroll" />
-//         <Navbar.Collapse id="navbarScroll">
-//           <Nav
-//             className="me-auto my-2 my-lg-0"
-//             style={{ maxHeight: '100px' }}
-//             navbarScroll
-//           >
-//             <Nav.Link href="#action1">Home</Nav.Link>
-//             <Nav.Link href="#action2">Link</Nav.Link>
-//             <NavDropdown title="Link" id="navbarScrollingDropdown">
-//               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-//               <NavDropdown.Item href="#action4">
-//                 Another action
-//               </NavDropdown.Item>
-//               <NavDropdown.Divider />
-//               <NavDropdown.Item href="#action5">
-//                 Something else here
-//               </NavDropdown.Item>
-//             </NavDropdown>
-//             <Nav.Link href="#" disabled>
-//               Link
-//             </Nav.Link>
-//           </Nav>
-//           <Form className="d-flex">
-//             <Form.Control
-//               type="search"
-//               placeholder="Search"
-//               className="me-2"
-//               aria-label="Search"
-//             />
-//             <Button variant="outline-success">Search</Button>
-//           </Form>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-// {/* ----------------------------Navbar end ---------------------------------------- */}
-// <Carousel data-bs-theme="dark">
-//       <Carousel.Item>
-//       <img src="public/lep1.jpg"  width="100%" height="500"/>
-//         <Carousel.Caption>
-//           <h5>HP Leptop</h5>
-//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//       <img src="public/lep2.jpg"  width="100%" height="500"/>        <Carousel.Caption>
-//           <h5>Second slide label</h5>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//       <img src="public/nav.avif"  width="100%" height="500"/>
-//         <Carousel.Caption>
-//           <h5>Third slide label</h5>
-//           <p>
-//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-//           </p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//     </Carousel>
-//     <br />
-//     <br />
-// {/* ---------------------------------carousel end ------------------------------------- */}
+          <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Arun Silawat</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+{/* ----------------------------Navbar end ---------------------------------------- */}
+<Carousel data-bs-theme="dark">
+      <Carousel.Item>
+      <img src="public/lep1.jpg"  width="100%" height="500"/>
+        <Carousel.Caption>
+          <h5>HP Leptop</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="public/lep2.jpg"  width="100%" height="500"/>        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="public/nav.avif"  width="100%" height="500"/>
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    <br />
+    <br />
+{/* ---------------------------------carousel end ------------------------------------- */}
 
 
 
 
-//     <Container fluid="md">
-//       <Row>
-//       <Col>
-//          <Card style={{ width: '18rem' }}>
-//          <img src="public/card1.jpg"  width="100%" height="200"/>
-//          <Card.Body>
-//          <Card.Title>Card Title</Card.Title>
-//          <Card.Text>
-//           Some quick example text to build.
-//          </Card.Text>
-//          </Card.Body>
-//          <Card.Body>
-//          <Card.Link href="#">Card Link</Card.Link>
-//          <Card.Link href="#">Another Link</Card.Link>
-//          </Card.Body>
-//          </Card>
-//        </Col>
+    <Container fluid="md">
+      <Row>
+      <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card1.jpg"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
 
-//        <Col>
-//          <Card style={{ width: '18rem' }}>
-//          <img src="public/card2.png"  width="100%" height="200"/>
-//          <Card.Body>
-//          <Card.Title>Card Title</Card.Title>
-//          <Card.Text>
-//           Some quick example text to build.
-//          </Card.Text>
-//          </Card.Body>
-//          <Card.Body>
-//          <Card.Link href="#">Card Link</Card.Link>
-//          <Card.Link href="#">Another Link</Card.Link>
-//          </Card.Body>
-//          </Card>
-//        </Col>
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card2.png"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
 
 
-//        <Col>
-//          <Card style={{ width: '18rem' }}>
-//          <img src="public/card3.webp"  width="100%" height="200"/>
-//          <Card.Body>
-//          <Card.Title>Card Title</Card.Title>
-//          <Card.Text>
-//           Some quick example text to build.
-//          </Card.Text>
-//          </Card.Body>
-//          <Card.Body>
-//          <Card.Link href="#">Card Link</Card.Link>
-//          <Card.Link href="#">Another Link</Card.Link>
-//          </Card.Body>
-//          </Card>
-//        </Col>
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card3.webp"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
   
-//        <Col>
-//          <Card style={{ width: '18rem' }}>
-//          <img src="public/card4.jpeg"  width="100%" height="200"/>
-//          <Card.Body>
-//          <Card.Title>Card Title</Card.Title>
-//          <Card.Text>
-//           Some quick example text to build.
-//          </Card.Text>
-//          </Card.Body>
-//          <Card.Body>
-//          <Card.Link href="#">Card Link</Card.Link>
-//          <Card.Link href="#">Another Link</Card.Link>
-//          </Card.Body>
-//          </Card>
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card4.jpeg"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
        </Col>
 
        <Col>
