a478afaf085e4d4ae64a71faff1f9e8c5d0dfce9
Arun silawat <arunsilawat722362@gmail.com>
2024-10-20 23:10:21 +0530

    react

--- React/useState.jsx
+++ React/useState.jsx
@@ -15,50 +15,50 @@ const App=()=>{
 export default App;
 //--------------------------------------------
 import { useState } from "react";
-// const App=()=>{
-//    const [name,setName]=useState("Arun");
-//    return(
-//       <>
-//       <h1>Hello We are Cybrom Student : {name}</h1>
-//       <button onClick={()=>{setName("Trun")}}>Click</button>
-//        </>
-//    )
-// }
-// export default App;
+const App=()=>{
+   const [name,setName]=useState("Arun");
+   return(
+      <>
+      <h1>Hello We are Cybrom Student : {name}</h1>
+      <button onClick={()=>{setName("Trun")}}>Click</button>
+       </>
+   )
+}
+export default App;
 
-// const App=()=>{
-//   const [color,setColor]=useState("Red");
-//   const myColor=()=>{
-//     setColor("Green")
-//   }
-//   return(
-//      <>
-//      <h1>My Favrite Color : {color}</h1>
-//      <button onClick={myColor}>Click</button>
-//       </>
-//   )
-// }
-// export default App;
-//--------------------------------------
+const App=()=>{
+  const [color,setColor]=useState("Red");
+  const myColor=()=>{
+    setColor("Green")
+  }
+  return(
+     <>
+     <h1>My Favrite Color : {color}</h1>
+     <button onClick={myColor}>Click</button>
+      </>
+  )
+}
+export default App;
+--------------------------------------
 
-// const App=()=>{
-//   const [color,setColor]=useState("Red");
+const App=()=>{
+  const [color,setColor]=useState("Red");
 
-//   return(
-//      <>
-//      <h1 style={{color:color}}>My Favrite Color : {color}</h1>
-//      <button onClick={()=>{setColor("Green")}}>Green</button>
-//      <button onClick={()=>{setColor("black")}}>Black</button>
-//      <button onClick={()=>{setColor("pink")}}>Pink</button>
-//      <button onClick={()=>{setColor("blue")}}>Blue</button>
-//      <button onClick={()=>{setColor("orange")}}>Orange</button>
+  return(
+     <>
+     <h1 style={{color:color}}>My Favrite Color : {color}</h1>
+     <button onClick={()=>{setColor("Green")}}>Green</button>
+     <button onClick={()=>{setColor("black")}}>Black</button>
+     <button onClick={()=>{setColor("pink")}}>Pink</button>
+     <button onClick={()=>{setColor("blue")}}>Blue</button>
+     <button onClick={()=>{setColor("orange")}}>Orange</button>
 
-//       </>
-//   )
-// }
-// export default App;
+      </>
+  )
+}
+export default App;
 
-//-------------------------------------------------
+-------------------------------------------------
 const App=()=>{
   const [cnt,setCnt]=useState(0);
 const myinc=()=>{
