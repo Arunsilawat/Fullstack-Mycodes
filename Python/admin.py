398dea00eccf6f118d2d73fbbd63e5ac2943627d
Arun silawat <arunsilawat722362@gmail.com>
2024-10-27 22:25:52 +0530

    py

---
+++ Python/admin.py
@@ -0,0 +1,7 @@
+# from django.contrib import admin
+
+# # Register your models here.
+# from.models import Aadhar,User,Department
+# admin.site.register(Aadhar)
+# admin.site.register(User)
+# admin.site.register(Department)
