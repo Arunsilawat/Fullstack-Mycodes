b25c4c69121db9c687970b5edb005cb8448f3d29
Arun silawat <arunsilawat722362@gmail.com>
2024-10-29 22:07:37 +0530

    py

--- Python/models.py
+++ Python/models.py
@@ -16,3 +16,23 @@ class Query(models.Model):
         return self.stu_query
 
         
+
+from django.db import models
+
+
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    contact=models.IntegerField()
+    def __str__(self):
+        return str(self.name)
+    
+
+quali=[("1","M.Tech"),("2","B.Tech"),("3","BCA"),("4","BBA")]
+
+class Profile(models.Model):
+    pro_name=models.OneToOneField(User,on_delete=models.CASCADE)
+    quali=models.CharField(max_length=50,choices=quali)
+    city=models.CharField(max_length=50)
+    def __str__(self):
+        return str(self.pro_name)
