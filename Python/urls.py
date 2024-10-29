f19ec20ee4fc17f9f6b159046ff8463f6fad0ec0
Arun silawat <arunsilawat722362@gmail.com>
2024-10-29 22:08:22 +0530

    py

--- Python/urls.py
+++ Python/urls.py
@@ -23,3 +23,12 @@ urlpatterns = [
     path('edit/<int:pk>',views.edit, name='edit'),
     path('update/<int:pk>',views.update, name='update'),
  ]
+ from django.contrib import admin
+from django.urls import path,include
+from app import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('userdata/', views.userdata, name='userdata'),
+    path('profiledata/', views.profiledata, name='profiledata'),
+    path('complatedata/', views.complatedata, name='complatedata'),
+]
